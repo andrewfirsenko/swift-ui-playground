04401a34a2b62de91e1f11461358fea546e5e72e
Andrew Firsenko <andrewfirsenko@gmail.com>
2025-02-05 14:50:57 +0300

    Init project

---
+++ SwiftUIPlayground/SwiftUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlaygroundApp.swift
+//  SwiftUIPlayground
+//
+//  Created by Andrew on 05.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
